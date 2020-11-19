79b669b69023ceea1260f5290a8f7dc6bb1b4a62
jay1934 <73715013+jay1934@users.noreply.github.com>
2020-11-19 11:55:15 -0500

    Switched from using a Map to using an Object
    I also removed the warning when a role is above the bot's as I'm pretty sure the bot role was moved all the way to the top last night

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,25 +1,14 @@
-module.exports = async ({ message, emoji, users }, user, { messages, id }) => {
-  const role = (messages.get(message.id) || new Map()).get(
-    emoji.id || emoji.name
-  );
+module.exports = async ({ message, emoji, users }, user, { messages }) => {
+  const role = (messages[message.id] || {})[emoji.id || emoji.name];
   if (user.id === user.client.user.id || !role) return;
   await users.remove(user);
   const { roles } = message.guild.member(user);
-  (roles.cache.has(role) ? roles.remove(role) : roles.add(role)).catch(() => {
-    if (
-      message.channel.messages.cache.some(
-        ({ author, content }) =>
-          author.id === id &&
-          content ===
-            "That role is above mine in the role hierarchy, so I can't assign/unassign it to/from any member."
-      )
-    )
-      return;
-    message.channel
-      .send(
-        `That role is above mine in the role hierarchy, so I can't assign/unassign it to/from any member.`
-      )
-      .then((msg) => msg.delete({ timeout: 5000 }))
-      .catch(() => {});
-  });
+  (roles.cache.has(role)
+    ? roles.remove(role)
+    : roles.add(role)
+  ).catch(() => {});
+  require('fs').writeFileSync(
+    './data/reactionDatabase.json',
+    JSON.stringify(messages, '', 2)
+  );
 };
