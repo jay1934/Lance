c4852259beabcc84439cec0985116c8e217eeb95
jay1934 <73715013+jay1934@users.noreply.github.com>
2020-11-19 11:57:13 -0500

    Switched from using a Map to using an Object

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,5 +1,10 @@
 module.exports = ({ message, emoji }, { id }, { messages, user }) => {
-  const _message = messages.get(message.id);
-  if (!_message || !_message.has(emoji.id || emoji.name)) return;
-  if (id === user.id) _message.delete(emoji.id || emoji.name);
+  const _message = messages[message.id];
+  if (!_message || !_message[emoji.id || emoji.name] || id !== user.id) return;
+  delete _message[emoji.id || emoji.name];
+  if (Object.values(_message).length === 1) delete messages[message.id];
+  require('fs').writeFileSync(
+    './data/reactionDatabase.json',
+    JSON.stringify(messages, '', 2)
+  );
 };
