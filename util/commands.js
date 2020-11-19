894265f9f944863f557e4263233c0116fc961dda
jay1934 <73715013+jay1934@users.noreply.github.com>
2020-11-19 12:38:19 -0500

    Why is this here

--- util/commands.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-dynamic-require */
-const { readdirSync } = require('fs');
-
-module.exports = (client) => {
-  readdirSync('./commands').forEach((file) => {
-    const command = require(`../commands/${file}`);
-    client.commands.set(command.name, command);
-  });
-};
